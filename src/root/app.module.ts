e336a3eaba9ef4b9a22ead454d9ab0ba697bdcd6
premSuperdev <108412360+premSuperdev@users.noreply.github.com>
2023-02-22 00:44:49 +0700

    add config

--- src/root/app.module.ts
+++ src/root/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+
+// import Component
+import { AppComponent } from './app.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { ContactComponent } from '../pages/contact/contact.component';
-
-import { TestCComponent } from '../components/test-c/test-c.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -19,7 +19,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, TestCComponent, HomeComponent, ContactComponent],
+  declarations: [AppComponent, HomeComponent, ContactComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
